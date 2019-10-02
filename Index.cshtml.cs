0daa8111951562b929747f2e9c552ff0963382ca
elTigre9 <33560876+elTigre9@users.noreply.github.com>
2019-10-01 19:33:41 -0500

    Add files via upload

---
+++ Index.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace application2.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string[] RobotNames { get; set; }
+        public void OnGet()
+        {
+            RobotNames = new string[] { "Litol Tigre", "Claudie B", "Litol NaNa", "Sugar Bear" };
+        }
+    }
+}
